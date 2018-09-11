635d3c56cccc342a94f38586fb13ac3e61f25f19
Sunyoung Hwang <sunyounghwang13@gmail.com>
2018-09-11 23:35:10 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,9 +3,9 @@
 
 def key_for_min_value(hash)
   unless hash.empty?
-    loop_count = 0
+    counter = 0
     hash.each do |key, num|
-      if loop_count == 0
+      if counter == 0
         min_num = num
         counter += 1
       else
